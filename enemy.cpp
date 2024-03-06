2237b828f083f70f7105e12e74283468aa8af73f
thaihoc <thaihoc131005@gmail.com>
2024-03-06 21:12:47 +0800

    enemy finish

--- enemy.cpp
+++ enemy.cpp
@@ -1,1 +1,33 @@
+#include "Enemy.h"
+
+Enemy::Enemy(int startX, int startY, int startVelX, int startVelY)
+    : x(startX),
+      y(startY),
+      velX(startVelX),
+      velY(startVelY) {}
+
+void Enemy::move() {
+    x += velX;
+    y += velY;
+
+    if (x < 0 || x + SQUARE_SIZE > SCREEN_WIDTH) {
+        velX = -velX;
+    }
+    if (y < 0 || y + SQUARE_SIZE > SCREEN_HEIGHT) {
+        velY = -velY;
+    }
+}
+
+void Enemy::move2() {
+    x += velX;
+    if (x < 0 || x + SQUARE_SIZE > SCREEN_WIDTH) {
+        velX = -velX;
+    }
+}
+
+void Enemy::render(SDL_Renderer* renderer) {
+    SDL_SetRenderDrawColor(renderer, 250, 0, 0, 0);
+    SDL_Rect enemyRect = {x, y, SQUARE_SIZE, SQUARE_SIZE};
+    SDL_RenderFillRect(renderer, &enemyRect);
+}
 
--- enemy.h
+++ enemy.h
@@ -1,1 +1,18 @@
+#ifndef ENEMY_H
+#define ENEMY_H
 
+#include <SDL.h>
+
+class Enemy {
+public:
+    int x, y;
+    double velX, velY;
+
+    Enemy(int startX, int startY, int startVelX, int startVelY);
+
+    void move();
+    void move2();
+    void render(SDL_Renderer* renderer);
+};
+
+#endif // ENEMY_H
