ad685302f9fffc223b17ca607be92e5dad63f80c
thaihoc <thaihoc131005@gmail.com>
2024-03-25 20:40:02 +0700

    feat(project): config time spawn

--- game.cpp
+++ game.cpp
@@ -17,7 +17,7 @@ Game::Game()
       lastPlayTime(0),
       font(nullptr),
       ENEMY_SPAWN_INTERVAL(5000),
-      ITEM_SPAWN_INTERVAL(3000),
+      ITEM_SPAWN_INTERVAL(10000),
       menuWidth(0),
       menuHeight(0),
       submenuWidth(0),
